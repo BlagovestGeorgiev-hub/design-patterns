de64f2e30c6903f6238361f96d9b0ece61311107
BlagovestGeorgiev-hub <55279815+BlagovestGeorgiev-hub@users.noreply.github.com>
2020-04-29 19:01:44 +0300

    Add files via upload

---
+++ MyCommandPattern/MyCommandMain.java
@@ -0,0 +1,9 @@
+package MyCommandPattern;
+
+public class MyCommandMain {
+    public static void main(String[] args) {
+        TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
+        textFileOperationExecutor.executeOperation(new OpenTextFileOperation(new TextFile("file1.txt")));
+        textFileOperationExecutor.executeOperation(new SaveTextFileOperation(new TextFile("file2.txt")));
+    }
+}
---
+++ MyCommandPattern/TextFileOperation.java
@@ -0,0 +1,5 @@
+package MyCommandPattern;
+
+public interface TextFileOperation {
+    String execute();
+}
